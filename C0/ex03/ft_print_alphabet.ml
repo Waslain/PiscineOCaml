dc3f287b5b4928fedfbe31906e52b7253ed972bb
Waslain <fduzant@Inception.myguest.virtualbox.org>
2025-07-16 22:22:20 +0200

    ex03

---
+++ C0/ex03/ft_print_alphabet.ml
@@ -0,0 +1,9 @@
+let ft_print_alphabet () =
+  let rec print2z c =
+    if c <= 'z' then (
+      print_char c;
+      print2z (char_of_int (int_of_char c + 1))
+    )
+  in
+  print2z 'a';
+  print_char '\n'
---
+++ C0/ex03/main.ml
@@ -0,0 +1,2 @@
+let () =
+  Ft_print_alphabet.ft_print_alphabet()
