fe4d1ccf82c3d10e42f9bb2f803bb306f086dfdb
Waslain <65572571+Waslain@users.noreply.github.com>
2025-12-01 13:48:53 +0100

    ex04

--- C02/ex04/nucleotides.ml
+++ C02/ex04/nucleotides.ml
@@ -1,3 +1,39 @@
-let nucleotides =
+type phosphate = string
+type deoxyribose = string
+type nucleobase =  A | T | C | G | None
+type nucleotide = phosphate * deoxyribose * nucleobase
 
-	
+let generate_nucleotide (c : char ) : nucleotide =
+	let base_type =
+		match c with
+		| 'A' -> A
+		| 'T' -> T
+		| 'C' -> C
+		| 'G' -> G
+		| _ -> None
+	in
+	let res : nucleotide = ("phosphate", "deoxyribose", base_type) in
+	res
+
+let () =
+	let print_test c =
+		Printf.printf "GenNucleotide %c  ⬇️\n" c;
+		let (p, d, b) = generate_nucleotide c in
+		let base_str = match b with
+				| A -> "A"
+				| T -> "T"
+				| C -> "C"
+				| G -> "G"
+				| None -> "None"
+		in
+		Printf.printf "Phosphate: %s, Deoxyribose: %s, Base: %s\n" p d base_str;
+		let expected =
+			Printf.sprintf "Phosphate: phosphate, Deoxyribose: deoxyribose, Base: %s" base_str
+		in
+		Printf.printf "%s\nExpected ------->  ⬆️\n\n" expected
+	in
+	print_test 'A';
+	print_test 'T';
+	print_test 'C';
+	print_test 'G';
+	print_test 'X'
