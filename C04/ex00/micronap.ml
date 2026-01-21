26b8baad6bc687fa4f7466653e65585d08740444
Waslain <65572571+Waslain@users.noreply.github.com>
2026-01-21 14:28:34 +0100

    fixed micronap and updated git ignore

--- C04/ex00/micronap.ml
+++ C04/ex00/micronap.ml
@@ -1,11 +1,11 @@
 let my_sleep () = Unix.sleep 1
 
 let main ac av =
-	if ac != 2 then exit 1;
-	for i = (int_of_string (Array.get av 1)) downto 1 do
+	if ac <> 2 then exit 1;
+	let x = (int_of_string (Array.get av 1)) in
+	for i = x downto 1 do
 		my_sleep()
 	done
 
 let () =
-	let av = Array.to_list Sys.argv in
-	main (List.length av) Sys.argv
+	main (Array.length Sys.argv) Sys.argv
